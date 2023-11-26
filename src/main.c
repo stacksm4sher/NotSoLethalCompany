0e1fe988e6fb5afd16187f132ce706a57acbbb88
devourmealive <foxmanlisichka@yandex.ru>
2023-11-27 02:12:59 +0300

    wrote DllMain func

---
+++ src/main.c
@@ -0,0 +1,25 @@
+#include <windows.h>
+
+#include "util.h"
+
+BOOL WINAPI
+DllMain(HINSTANCE hinstDLL,  // handle to DLL module
+        DWORD fdwReason,     // reason for calling function
+        LPVOID lpvReserved)
+{
+    if (fdwReason == DLL_PROCESS_ATTACH)
+    {
+        DisableThreadLibraryCalls(hinstDLL);
+
+        pHinstance = hinstDLL;
+
+        HANDLE thread = CreateThread(NULL, 0, Hook, 0, 0, NULL);
+
+        if (thread)
+        {
+            CloseHandle(thread);
+        }
+    }
+
+    return TRUE;
+}
