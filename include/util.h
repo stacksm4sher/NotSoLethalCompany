9da880a78d22d1afbb4ac095fdaa87d588edfee1
devourmealive <foxmanlisichka@yandex.ru>
2023-11-28 02:29:14 +0300

    added vtable/static_field_value/property getters, runtime class init method, MonoProperty structure, GetStaticFieldValue and GetProperty methods

--- include/util.h
+++ include/util.h
@@ -27,20 +27,47 @@ typedef void *(*t_mono_field_get_value)(const void *domain, const void *field, c
 
 typedef void *(*t_mono_array_length)(void *array);
 
+typedef void *(*t_mono_class_vtable)(void *domain, void *clazz);
+
+typedef void (*t_mono_field_static_get_value) (void *vt, void *field, void *pValue);
+
+typedef void (*t_mono_runtime_class_init) (void *vt);
+
+typedef void *(*t_mono_class_get_property_from_name)(void *clazz, const char *name);
+
+typedef struct MonoProperty
+{
+    void *parentClazz;
+    const char *name;
+    void *getMethod;
+    void *setMethod;
+    unsigned int attrs;
+} monoProperty;
+
 const static char *GAME_ASSEMBLY = "ENTER YOUR PATH TO Assembly-CSharp.dll";
 const static char *UNITY_ENGINE = "UnityEngine.dll";
 const static char *MSCORLIB = "mscorlib.dll";
 
-static HINSTANCE pHinstance;
+extern HINSTANCE pHinstance;
 
 void *
 GetMethod(const char *clazzName, const char *namespace, const char *methodName, const char *assembly, int argsCnt);
 
 void *
-GetFieldValue(const char *clazzName, const char *fieldName, const char *namespace, const char *assembly, const void *object);
+GetFieldValue(const char *clazzName, const char *fieldName, const char *namespace, const char *assembly, const void *object, unsigned int fieldSize);
+
+void *
+GetStaticFieldValue(const char *clazzName,
+                    const char *fieldName,
+                    const char *namespace,
+                    const char *assembly,
+                    unsigned int fieldSize);
 
 void *
 GetClazz(const char *clazzName, const char *namespace, const char *assembly);
+
+void *
+GetProperty(void *clazz, const char *name);
 
 DWORD WINAPI
 Hook(LPVOID lpParam);
